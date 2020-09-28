c6510517e1b001ee4467d85af0df24862f8b583b
yiminprogram <minwork1016@outlook.com>
2020-09-28 23:25:41 +0800

    Updates

---
+++ precache-manifest.c4ea30430fc32d0372163c862cfae492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f0b27431805c4e427107f9b7fda269",
+    "url": "/react-facebook-clone/index.html"
+  },
+  {
+    "revision": "d1033d1f66353c76308a",
+    "url": "/react-facebook-clone/static/js/2.8e5eebdc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-facebook-clone/static/js/2.8e5eebdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124c12ad669a57f95295",
+    "url": "/react-facebook-clone/static/js/main.a46c9e89.chunk.js"
+  },
+  {
+    "revision": "01110c454a91783259dd",
+    "url": "/react-facebook-clone/static/js/runtime-main.2e9ff287.js"
+  },
+  {
+    "revision": "2f093f732284db0cacd25a10171ce4c0",
+    "url": "/react-facebook-clone/static/media/user-photo.2f093f73.png"
+  }
+]);
